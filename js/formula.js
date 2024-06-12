448f7a1c80e2eedf36701154809ca7ccce305287
Thomas Reichmann <thomasalmeidar@gmail.com>
2024-06-12 13:16:26 -0300

    alerta do form de contato

--- js/formula.js
+++ js/formula.js
@@ -10,5 +10,33 @@ function handleFormAposta(event) {
     alert("Sua aposta foi feita, fique ligado para ver seus resultados")
 }
 
+/**
+ * @param event {SubmitEvent}
+ */
+function handleFormContato(event) {
+    event.preventDefault()
+
+    // Resetar os campos do form
+    event.currentTarget.reset()
+
+    alert("Sua mensagem foi enviada com sucesso! Entreremos em contato em breve.")
+}
+
 document.getElementById("form-equipe").addEventListener("submit", handleFormAposta)
 document.getElementById("form-piloto").addEventListener("submit", handleFormAposta)
+
+document.getElementById("form-contato").addEventListener("submit", handleFormContato)
+
+function setCustomValidationMessages(id, message) {
+    const element = document.getElementById(id);
+    element.oninvalid = function (event) {
+        event.target.setCustomValidity(message);
+    };
+    element.oninput = function (event) {
+        event.target.setCustomValidity('');
+    };
+}
+
+setCustomValidationMessages('nome', 'Por favor, preencha este campo.');
+setCustomValidationMessages('email', 'Por favor, insira um endereço de e-mail válido que contenha um @.');
+setCustomValidationMessages('mensagem', 'Por favor, preencha este campo.');
